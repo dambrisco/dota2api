9edf6c12a0af331b0f6863827a446d97c354c317
Dan Ambrisco <dambrisco@gmail.com>
2017-01-19 15:09:14 -0700

    Break api into module, rename to dota2api

--- examples/fetch-id-by-username.rs
+++ examples/fetch-id-by-username.rs
@@ -1,13 +1,15 @@
-extern crate dota2api_rust;
+extern crate dota2api;
 
 use std::env::*;
+use dota2api::api::Api;
 
 fn main() {
     let key = match var("STEAM_API_KEY") {
         Ok(key) => key,
         Err(_) => panic!("You must have a STEAM_API_KEY environment configured to use this example"),
     };
+    let api = Api::new(key);
     let vanity = args().skip(1).take(1).next().unwrap();
-    let steamid = dota2api_rust::resolve_vanity_url(key, vanity);
+    let steamid = api.resolve_vanity_url(vanity);
     println!("{}", steamid);
 }
