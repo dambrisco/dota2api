fe6faa18858567d308819ba638eab493ce9c7749
Dan Ambrisco <dambrisco@gmail.com>
2017-04-30 21:32:49 -0600

    Cleanup match history example

--- examples/get_match_history.rs
+++ examples/get_match_history.rs
@@ -1,9 +1,7 @@
 extern crate dota2api;
 
-use std::env::{ var, args };
+use std::env::{ var };
 use dota2api::Api;
-use std::fmt;
-use dota2api::response::MatchHistory;
 
 fn main() {
     let key = match var("STEAM_API_KEY") {
@@ -11,7 +9,6 @@ fn main() {
         Err(_) => panic!("You must have a STEAM_API_KEY environment configured to use this example"),
     };
     let api = Api::new(key);
-    let vanity = args().skip(1).take(1).next().unwrap();
     match api.get_match_history(Vec::new()) {
         Ok(history) => println!("{}", history.result.matches.len()),
         Err(ref e) => println!("{}: {}", e.get_type(), e)
